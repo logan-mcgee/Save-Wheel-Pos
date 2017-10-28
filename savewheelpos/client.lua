9cea69bca9e38a23eaef157431e2f7d021ec114c
IllusiveTea <31740751+IllusiveTea@users.noreply.github.com>
2017-10-28 16:14:49 +0100

    Add files via upload

---
+++ savewheelpos/client.lua
@@ -0,0 +1,15 @@
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        if IsPedInAnyVehicle(PlayerPedId(), true) then
+            veh = GetVehiclePedIsUsing(PlayerPedId())
+            angle = GetVehicleSteeringAngle(veh)
+            veh2 = GetPlayersLastVehicle()
+            sped = GetEntitySpeed(veh)
+            Citizen.Wait(20)
+            if sped < 10 then
+                SetVehicleSteeringAngle(veh2, angle)
+            end
+        end
+    end
+end)
