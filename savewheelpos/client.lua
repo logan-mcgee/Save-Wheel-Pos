06694414a4805371a9a852c6531614c854d678ae
IllusiveTea <illusivetea@outlook.com>
2018-01-06 22:33:43 +0000

    Updated to now work with multiple vehicles, and stop bike glitching out, thanks to Vespura!

--- savewheelpos/client.lua
+++ savewheelpos/client.lua
@@ -1,14 +1,17 @@
 Citizen.CreateThread(function()
+    local angle = 0.0
+    local speed = 0.0
     while true do
         Citizen.Wait(0)
-        if IsPedInAnyVehicle(PlayerPedId(), true) then
-            veh = GetVehiclePedIsUsing(PlayerPedId())
-            angle = GetVehicleSteeringAngle(veh)
-            veh2 = GetPlayersLastVehicle()
-            sped = GetEntitySpeed(veh)
-            Citizen.Wait(50)
-            if sped < 10 then
-                SetVehicleSteeringAngle(veh2, angle)
+        local veh = GetVehiclePedIsUsing(PlayerPedId())
+        if DoesEntityExist(veh) then
+            local tempangle = GetVehicleSteeringAngle(veh)
+            if tempangle > 10.0 or tempangle < -10.0 then
+                angle = tempangle
+            end
+            speed = GetEntitySpeed(veh)
+            if speed < 2.0 and DoesEntityExist(GetVehiclePedIsIn(PlayerPedId(), true)) then
+                SetVehicleSteeringAngle(GetVehiclePedIsIn(PlayerPedId(), true), angle)
             end
         end
     end
